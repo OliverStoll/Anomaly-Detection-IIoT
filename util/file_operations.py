d75c11b9c2e75ca5ff91e570ff12444931741933
OliverStoll <oliverstoll.berlin@gmail.com>
2022-05-19 22:38:05 +0200

    Added Data Pipeline for bearing dataset to clean and label data. Measurements are now split into one csv each.

--- util/file_operations.py
+++ util/file_operations.py
@@ -2,7 +2,28 @@ import numpy as np
 import pandas as pd
 import os
 import datetime
+from matplotlib import pyplot as plt
 
+
+files_kbm = ["stabilus", "stadtkehl", "wasserwerke-a", "wasserwerke-b", "piaggio", "gummipumpe", "pumpe-v2", "pumpe-v3"]
+
+anomalies_kbm = {'stabilus': ["17/06/19 10:22:00",
+                                  "17/06/19 11:22:00",
+                                  "17/06/19 12:24:00",
+                                  "17/06/19 13:24:00"],
+                 'stadtkehl': ["22/05/19 06:39:59"
+                               "22/05/19 07:40:36",
+                               "22/05/19 08:41:13",
+                               "22/05/19 09:41:51"
+                               ],
+                 'pumpe-v2': ["2/16/18 17:59",
+                              "2/16/18 18:59"],
+                 'pumpe-v3': ["5/20/18 7:35",
+                              "5/25/18 18:35"]}
+
+anomalies_bearing = {'test1': [1.000, 0.985, 0.840, 0.665],
+                     'test2': [0.710, 0.980, 1.000, 0.900],
+                     'test3': [0.980, 0.975, 0.972, 0.972]}
 
 class Resampler:
     """
@@ -11,10 +32,9 @@ class Resampler:
     The class has two methods for the two datasets used, bearings and kbm.
     """
 
-    def __init__(self, data_path, num_features):
+    def __init__(self, data_path):
         self.data_path = data_path
         self.dataset = "kbm" if "kbm" in data_path else "bearing"
-        self.num_features = num_features
 
     def resample_default(self):
         # resample data to default sampling sizes
@@ -34,7 +54,7 @@ class Resampler:
         else:
             self.resample_data_bearing(resample_size)
 
-    def resample_data_bearing(self, resample_size):
+    def resample_data_bearing(self, resample_size=20480):
         """
         Resample data from KBM dataset.
 
@@ -49,11 +69,11 @@ class Resampler:
             for df_chunk in dfs:
                 # each new sample is down-sampled by taking the mean of the dataframe
                 mean_abs = np.array(df_chunk.abs().mean())
-                mean_abs = pd.DataFrame(mean_abs.reshape(1, 4))
+                mean_abs = pd.DataFrame(mean_abs.reshape(1, -1))
                 # append the new sample to the file
-                mean_abs.to_csv(f"{self.data_path}_{resample_size}.csv", mode='a', index=False, header=False, sep=';')
+                mean_abs.to_csv(f"{self.data_path}_{resample_size}.csv", mode='a', index=False, header=False, sep=',')
 
-    def resample_data_kbm(self, resample_size, sampling_rate=4000):  # todo: use timestamps instead of sampling rate
+    def resample_data_kbm(self, resample_size, sampling_rate=800):
         """
         Resample data from KBM dataset.
 
@@ -85,7 +105,7 @@ class Resampler:
             for df_chunk in dfs:
                 # down-sample the measurement by taking the mean of each chunk
                 mean_abs = np.array(df_chunk.abs().mean())
-                mean_abs = pd.DataFrame(mean_abs.reshape(1, self.num_features))
+                mean_abs = pd.DataFrame(mean_abs.reshape(1, -1))
                 mean_abs.to_csv(save_path, mode='a', index=False, header=False, sep=';')
 
 
@@ -153,7 +173,7 @@ class DataCleanerKBM:
                 elif time_dif == datetime.timedelta(seconds=1):
                     print(file_name, time, time_dif)
 
-    def add_label_kbm(self, excel_timestamps=True):
+    def add_label(self, excel_timestamps=True):
 
         # iterate over all file names
         for file_name in self.file_names:
@@ -180,26 +200,89 @@ class DataCleanerKBM:
             df.to_csv(f"data/kbm_dataset/{file_name}_labeled.csv", index=False)
 
 
+class DataCleanerBearingTest:
+    def __init__(self, data_path, num_features=1):
+        self.data_path = data_path
+        self.archive_path = "archive/" + self.data_path
+        self.num_features = num_features
+        self.num_bearings = 4
+        print(f"DataCleanerBearing: {self.data_path}")
+
+    def clean_data(self):
+        """
+        Loads all csv files from the data path and creates a dataframe containing all data from this test-run.
+
+        The files contain one or multiple features from multiple bearings that were being tested simultaneously.
+        """
+
+        # get all files from the path and concatenate them to a single csv file
+        for file in os.scandir(self.archive_path):
+            df = pd.read_csv(f"{self.archive_path}/{file.name}", sep='\t', header=None)
+            df.to_csv(self.data_path + "_original.csv", mode='a', index=False, header=False)  # needs to not exist yet!!
+
+        # read csv file from path
+        df = pd.read_csv(self.data_path + "_original.csv", header=None)
+
+        # take the created csv file and split it into multiple files containing one bearing.
+        for i in range(self.num_bearings):
+            print(i)
+            df_bearing = df.iloc[:, i * self.num_features:(i + 1) * self.num_features]  # select num_features columns
+            # name the columns and save the dataframe
+            df_bearing.columns = ["Vibration-" + str(i) for i in range(df_bearing.shape[1])]
+            df_bearing.to_csv(f"{self.data_path}_bearing-{i}_original.csv", index=False)
+
+    def plot_data(self):
+        """
+        Plots the raw vibration data from the test-run. This allows a visual anotation of the data.
+
+        This method is obviously far from perfect for accurate anomaly detection and is only usable as the data is
+        clearly defined as 'run until failure'.
+        """
+
+        # read csv files from path
+        for i in range(self.num_bearings):
+            df = pd.read_csv(f"{self.data_path}_bearing-{i}_original.csv", header=None)
+            # create figure
+            plt.figure(figsize=(20, 20))
+            plt.plot(df)
+            for i in range(50):
+                # draw vertical lines of the x-axis for visual inspection of anomalies
+                if i % 5 == 0:
+                    plt.axvline(i * int(df.shape[0] / 50), color='green', linestyle='-')
+                else:
+                    plt.axvline(i * int(df.shape[0] / 50), color='r', linestyle='-')
+            plt.show()
+
+    def add_label(self, time_percentile_list):
+        """
+        Add label if a bearing data is anomalious (yet) or not, where 1 symbolise anomalies.
+
+        Since all tests were 'run until failure', when a bearing is anomalious it will stay this way.
+        The time_percentile is used to determine at which point the data is considered anomalius.
+
+        :param time_percentile_list: list of the fraction of the total time at which anomalious behaviour starts.
+        """
+
+        assert len(time_percentile_list) == self.num_bearings
+
+        # iterate over all bearing files and add label depending on time
+        for i in range(self.num_bearings):
+            df = pd.read_csv(f"{self.data_path}_bearing-{i}_original.csv")
+
+            # set all anomaly values to 1 for the second half of the dataframe
+            start_index = int(df.shape[0] * time_percentile_list[i])
+            df['anomaly'] = 0
+            df['anomaly'][start_index:] = 1
+
+            # save the labeled data
+            df.to_csv(f"{self.data_path}_bearing-{i}.csv", index=False)
+
+
 if __name__ == '__main__':
-    # clean_csv_kbm(path="archive/data/kbm_dataset/New folder/abc-Piaggio/data.csv", name="piaggio", sep=',')
 
-    files = ["stabilus", "stadtkehl", "wasserwerke-a", "wasserwerke-b", "piaggio", "gummipumpe", "pumpe-v2", "pumpe-v3"]
+    # DataCleanerKBM(file_names=files, data_path="data/kbm_dataset", anomaly_timestamps=anomaly_ts).check_data()
 
-    anomaly_ts = {'stabilus': ["17/06/19 10:22:00",
-                               "17/06/19 11:22:00",
-                               "17/06/19 12:24:00",
-                               "17/06/19 13:24:00"],
-                  'stadtkehl': ["22/05/19 06:39:59"
-                                "22/05/19 07:40:36",
-                                "22/05/19 08:41:13",
-                                "22/05/19 09:41:51"
-                                ],
-                  'pumpe-v2': ["2/16/18 17:59",
-                               "2/16/18 18:59"],
-                  'pumpe-v3': ["5/20/18 7:35",
-                               "5/25/18 18:35"]}
+    DataCleanerBearingTest(data_path="data/bearing_dataset/test1", num_features=2).add_label(anomalies_bearing['test1'])
+    DataCleanerBearingTest(data_path="data/bearing_dataset/test2").add_label(anomalies_bearing['test2'])
 
-    datacleaner = DataCleanerKBM(file_names=files, data_path="data/kbm_dataset", anomaly_timestamps=anomaly_ts)
-    datacleaner.check_data()
-
-    # Resampler(data_path='data/kbm_dataset/pumpe_v3', num_features=5).resample_default()
+    # Resampler(data_path='archive/data/bearing_dataset/test1').resample_data(resample_size=20480)
